32fdb3d8f17508335e6b5b3e1371a9f722aca3ae
Jean Francisco dos Santos Fernandes <101561495+JeanOmeg@users.noreply.github.com>
2022-07-16 16:29:37 -0300

    Desafio java

---
+++ desafio-padroes-projetos/Main.java
@@ -0,0 +1,55 @@
+import facade.Facade;
+import singleton.SingletonEager;
+import singleton.SingletonLazy;
+import singleton.SingletonLazyHolder;
+import strategy.Comportamento;
+import strategy.ComportamentoAgressivo;
+import strategy.ComportamentoDefensivo;
+import strategy.ComportamentoNormal;
+import strategy.Robo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// Singleton
+		System.out.println("=====Singleton=====");
+		SingletonLazy lazy = SingletonLazy.getInstancia();
+		System.out.println(lazy);
+		lazy = SingletonLazy.getInstancia();
+		System.out.println(lazy);
+
+		SingletonEager eager = SingletonEager.getInstancia();
+		System.out.println(eager);
+		eager = SingletonEager.getInstancia();
+		System.out.println(eager);
+		
+		SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstancia();
+		System.out.println(lazyHolder);
+		lazyHolder = SingletonLazyHolder.getInstancia();
+		System.out.println(lazyHolder);
+		
+		// Strategy
+		System.out.println("\n=====Strategy=====");
+		Comportamento defensivo = new ComportamentoDefensivo();
+		Comportamento normal = new ComportamentoNormal();
+		Comportamento agressivo = new ComportamentoAgressivo();
+		
+		Robo robo = new Robo();
+		robo.setComportamento(normal);
+		robo.mover();
+		robo.mover();
+		robo.setComportamento(defensivo);
+		robo.mover();
+		robo.setComportamento(agressivo);
+		robo.mover();
+		robo.mover();
+		robo.mover();
+		
+		// Facade
+		System.out.println("\n=====Facade=====");
+		Facade facade = new Facade();
+		facade.migrarCliente("Jean", "80045270");
+	}
+
+}
